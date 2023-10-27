98aff8108917b1276bdf46851c06dac1ab69cee4
surajitr1 <surajitr1@vmware.com>
2023-10-28 00:14:28 +0530

    feat[auth-reducer]: updated auth-reducer to catch errors

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,12 +1,13 @@
-import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { createActionGroup, props } from '@ngrx/store';
 import { RegisterRequestInterface } from '../types/register-request.interface';
-import { CurrentUserInterface } from 'src/app/shared/types/current-user.interface';
+import { CurrentUserInterface } from '../../shared/types/current-user.interface';
+import { BackendErrorsInterface } from '../../shared/types/backend-errors.interface';
 
 export const authActions = createActionGroup({
   source: 'auth',
   events: {
     Register: props<{ request: RegisterRequestInterface }>(),
     'Register success': props<{ currentUser: CurrentUserInterface }>(),
-    'Register failure': emptyProps(),
+    'Register failure': props<{ errors: BackendErrorsInterface }>(),
   },
 });
--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -3,7 +3,8 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AuthService } from '../services/auth.service';
 import { authActions } from './actions';
 import { catchError, map, of, switchMap } from 'rxjs';
-import { CurrentUserInterface } from 'src/app/shared/types/current-user.interface';
+import { CurrentUserInterface } from '../../shared/types/current-user.interface';
+import { HttpErrorResponse } from '@angular/common/http';
 
 export const registerEffect = createEffect(
   (actions$ = inject(Actions), authService = inject(AuthService)) => {
@@ -14,8 +15,12 @@ export const registerEffect = createEffect(
           map((currentUser: CurrentUserInterface) => {
             return authActions.registerSuccess({ currentUser });
           }),
-          catchError(() => {
-            return of(authActions.registerFailure());
+          catchError((errorResponse: HttpErrorResponse) => {
+            return of(
+              authActions.registerFailure({
+                errors: errorResponse.error.errors,
+              })
+            );
           })
         );
       })
