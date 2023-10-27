83048e9bf78374bd8039b946ffeec199ba0d1f1d
surajitr1 <surajitr1@vmware.com>
2023-10-27 15:04:02 +0530

    feat[code-cleanup]: use ng standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports: [RouterOutlet],
 })
-export class AppComponent {
-  title = 'ngrx-medium-clone-app';
-}
+export class AppComponent {}
